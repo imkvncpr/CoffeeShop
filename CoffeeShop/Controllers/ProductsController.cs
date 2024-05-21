5539055ea7750112b86ee5404aeb7b2544f36895
Kevon Cooper <imkvncpr@gmail.com>
2024-05-21 11:56:18 -0400

    Add project files.

---
+++ CoffeeShop/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Models.Interfaces;
+using CoffeeShop.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductsController(IProductRepository ProductRespository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts);
+        }
+
+    }
+}
---
+++ CoffeeShop/Models/Services/ProductRespository.cs
@@ -0,0 +1,30 @@
+﻿using CoffeeShop.Models.Interfaces;
+
+namespace CoffeeShop.Models.Services
+{
+    public class ProductRespository : IProductRepository
+    {
+        private List<Product> ProductList = new List<Product>()
+        {
+            new Product {Id = 1, Name = "Americano", Price =25, ProductDetail = "Meh", ImageUrl = ""},
+            new Product {Id = 2, Name = "Mocha", Price = 30, ProductDetail = "Also Meh", ImageUrl = ""}
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return ProductList;
+        }
+
+        public Product GetProductDetails(int id)
+        {
+            return ProductList.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetTrendingProducts()
+        {
+            return ProductList.Where(p => p.IsTrendingProduct);
+        }
+    };
+      
+}
+    
+
